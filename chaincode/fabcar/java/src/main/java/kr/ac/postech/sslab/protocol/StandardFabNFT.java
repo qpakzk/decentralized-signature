4d79284e287d9414caab138cf125277d920cc738
Sangwon Hong <qpakzk@gmail.com>
2019-07-16 03:47:50 -0700

    add StandardFabNFT interface

---
+++ chaincode/fabcar/java/src/main/java/kr/ac/postech/sslab/protocol/StandardFabNFT.java
@@ -0,0 +1,10 @@
+package kr.ac.postech.sslab.protocol;
+
+import org.hyperledger.fabric.shim.Chaincode.Response;
+import org.hyperledger.fabric.shim.ChaincodeStub;
+import java.util.List;
+
+public interface StandardFabNFT {
+    public Response ownerOf(ChaincodeStub stub, List<String> args);
+    public Response transferFrom(ChaincodeStub stub, List<String> args);
+}
