0655068d72806fb9f59900a29ada9d60a551d409
Sangwon Hong <qpakzk@gmail.com>
2019-08-01 22:56:44 +0900

    change null to blank string

--- chaincode/fabcar/java/src/main/java/kr/ac/postech/sslab/type/NFT.java
+++ chaincode/fabcar/java/src/main/java/kr/ac/postech/sslab/type/NFT.java
@@ -33,8 +33,8 @@ public class NFT {
 
     private void mint(ChaincodeStub stub, String id, String hash, String uri) {
         this.id = id;
-        this.owner = null;
-        this.operator = null;
+        this.owner = "";
+        this.operator = "";
         this.hash = hash;
         this.uri = uri;
         this.isActivated = true;
