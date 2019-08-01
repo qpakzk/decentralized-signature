83d5a2af1b0deef0917c7cf856f5af97f8da997c
Sangwon Hong <qpakzk@gmail.com>
2019-08-01 20:21:36 +0900

    implement Extension

--- chaincode/fabcar/java/src/main/java/kr/ac/postech/sslab/protocol/EERC721.java
+++ chaincode/fabcar/java/src/main/java/kr/ac/postech/sslab/protocol/EERC721.java
@@ -1,45 +1,71 @@
 package kr.ac.postech.sslab.protocol;
 
+import kr.ac.postech.sslab.msg_type.MsgEditNFTMetadata;
+import kr.ac.postech.sslab.msg_type.MsgMintNFT;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.hyperledger.fabric.shim.ChaincodeStub;
 import java.util.List;
 
-import kr.ac.postech.sslab.structure.TokenManager;
-
 public class EERC721 extends ERC721 implements IEERC721 {
-    private String tokenType;
+	private static Log _logger = LogFactory.getLog(EERC721.class);
+	private FabNFTManager nftManager;
 
-    public EERC721() {
-        super();
-    }
+	public EERC721() {
+		super();
+		this.nftManager = super.getNFTManager();
+	}
 
 	@Override
 	public Response init(ChaincodeStub stub) {
-        return newSuccessResponse();
+		try {
+			_logger.info("Init java EERC721 chaincode");
+			String func = stub.getFunction();
+
+			if (!func.equals("init")) {
+				return newErrorResponse("Method other than init is not supported");
+			}
+
+			List<String> args = stub.getParameters();
+			if (args.size() != 0) {
+				newErrorResponse("Incorrect number of arguments. Expecting 0");
+			}
+
+			return newSuccessResponse();
+		} catch (Throwable e) {
+			return newErrorResponse(e);
+		}
 	}
 	
     @Override
     public Response invoke(ChaincodeStub stub) {
-
-        try {
-            String func = stub.getFunction();
+		try {
+			_logger.info("Invoke java EERC721 chaincode");
+			String func = stub.getFunction();
 			List<String> args = stub.getParameters();
-			
-			if (func.equals("balanceOf")) {
+
+			if(func.equals("balanceOf")) {
 				return super.balanceOf(stub, args);
 			}
-			else if (func.equals("ownerOf")) {
+			else if(func.equals("ownerOf")) {
 				return super.ownerOf(stub, args);
 			}
 			else if(func.equals("transferFrom")) {
 				return super.transferFrom(stub, args);
 			}
-			else if (func.equals("approve")) {
+			else if(func.equals("approve")) {
 				return super.approve(stub, args);
 			}
-			else if (func.equals("initialize")) {
-				return this.initialize(stub, args);
+			else if(func.equals("setApprovalForAll")) {
+				return super.setApprovalForAll(stub, args);
 			}
-            else if (func.equals("divide")) {
+			else if(func.equals("getApproved")) {
+				return super.getApproved(stub, args);
+			}
+			else if(func.equals("isApprovedForAll")) {
+				return super.isApprovedForAll(stub, args);
+			}
+			else if (func.equals("divide")) {
 				return this.divide(stub, args);
 			}
 			else if (func.equals("delete")) {
@@ -55,67 +81,95 @@ public class EERC721 extends ERC721 implements IEERC721 {
 				return this.queryTokenHistory(stub, args);
 			}
 
-			return newErrorResponse("Invalid invoke function name.");
+			return newErrorResponse("Invalid invoke method name. Expecting one of: "
+					+ "[\"balanceOf\", \"ownerOf\", \"transferFrom\", \"approve\", \"setApprovalForAll\", \"getApproved\", \"isApprovedForAll\", "
+					+ "\"divide\", \"delete\", \"update\", \"query\", \"queryTokenHistory\"]");
 		} catch (Throwable e) {
-			return newErrorResponse(e.getMessage());
+			return newErrorResponse(e);
 		}
 	}
 
 	@Override
-	public Response initialize(ChaincodeStub stub, List<String> args) {
-		try {
-			TokenManager tokenManager = new TokenManager(this.tokenType);
-			tokenManager.initialize(stub, super.getRecentCreatedTokenId());
-
-            return newSuccessResponse("Succeeded 'initialize' function");
-        } catch (Throwable e) {
-            return newErrorResponse("Failed 'initialize' function");
+	public Response mint(ChaincodeStub stub, List<String> args) {
+		if(args.size() != 2) {
+			return newErrorResponse("Incorrect number of arguments. Expecting 2");
 		}
+
+		String _hash = args.get(0);
+		String _uri = args.get(1);
+
+		MsgMintNFT msg = new MsgMintNFT(_hash, _uri);
+
+		String nftString = this.nftManager.mint(stub, msg);
+
+		if(nftString == null) {
+			return newErrorResponse("NFT not minted.");
+		}
+
+		return newSuccessResponse(nftString);
 	}
 
 	@Override
 	public Response divide(ChaincodeStub stub, List<String> args) {
-		try {
-            return newSuccessResponse("Succeeded 'divide' function");
-        } catch (Throwable e) {
-            return newErrorResponse("Failed 'divide' function");
+		if(args.size() != 1) {
+			return newErrorResponse("Incorrect number of arguments. Expecting 1");
 		}
+
+		String _tokenId = args.get(0);
+		return newSuccessResponse();
 	}
 
 	@Override
     public Response delete(ChaincodeStub stub, List<String> args) {
-		try {
-            return newSuccessResponse("Succeeded 'delete' function");
-        } catch (Throwable e) {
-            return newErrorResponse("Failed 'delete' function");
+		if(args.size() != 1) {
+			return newErrorResponse("Incorrect number of arguments. Expecting 1");
 		}
+
+		String _tokenId = args.get(0);
+
+		nftManager.deactivate(stub, _tokenId);
+		return newSuccessResponse();
 	}
 
 	@Override
     public Response update(ChaincodeStub stub, List<String> args) {
-		try {
-            return newSuccessResponse("Succeeded 'update' function");
-        } catch (Throwable e) {
-            return newErrorResponse("Failed 'update' function");
+		if(args.size() != 3) {
+			return newErrorResponse("Incorrect number of arguments. Expecting 3");
 		}
+
+		String _hash = args.get(0);
+		String _uri = args.get(1);
+		String _tokenId = args.get(2);
+
+		MsgEditNFTMetadata msg = new MsgEditNFTMetadata(_tokenId, _hash, _uri);
+		nftManager.edit(stub, msg);
+		return newSuccessResponse();
 	}
 
 	@Override
     public Response query(ChaincodeStub stub, List<String> args) {
-		try {
-            return newSuccessResponse("Succeeded 'query' function");
-        } catch (Throwable e) {
-            return newErrorResponse("Failed 'query' function");
+		if(args.size() != 1) {
+			return newErrorResponse("Incorrect number of arguments. Expecting 1");
 		}
+
+		String _tokenId = args.get(0);
+
+		String nftString = nftManager.query(stub, _tokenId);
+
+		return newSuccessResponse(nftString);
 	}
 
 	@Override
     public Response queryTokenHistory(ChaincodeStub stub, List<String> args) {
-		try {
-            return newSuccessResponse("Succeeded 'queryTokenHistory' function");
-        } catch (Throwable e) {
-            return newErrorResponse("Failed 'queryTokenHistory' function");
+		if(args.size() != 1) {
+			return newErrorResponse("Incorrect number of arguments. Expecting 1");
 		}
+
+		String _tokenId = args.get(0);
+
+		String histories = nftManager.queryHistory(stub, _tokenId);
+
+		return newSuccessResponse(histories);
 	}
 
 	public static void main(String[] args) {
--- chaincode/fabcar/java/src/main/java/kr/ac/postech/sslab/protocol/IEERC721.java
+++ chaincode/fabcar/java/src/main/java/kr/ac/postech/sslab/protocol/IEERC721.java
@@ -5,7 +5,7 @@ import org.hyperledger.fabric.shim.ChaincodeStub;
 import java.util.List;
 
 public interface IEERC721 {
-    public Response initialize(ChaincodeStub stub, List<String> args);
+    public Response mint(ChaincodeStub stub, List<String> args);
     public Response divide(ChaincodeStub stub, List<String> args);
     public Response delete(ChaincodeStub stub, List<String> args);
     public Response update(ChaincodeStub stub, List<String> args);
