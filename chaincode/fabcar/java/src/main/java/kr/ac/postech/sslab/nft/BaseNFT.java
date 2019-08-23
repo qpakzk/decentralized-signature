8833612579da69466286850328515daa095bef55
Sangwon Hong <qpakzk@gmail.com>
2019-08-23 22:04:29 +0900

    reimplement BaseNFT depending on a new architecture

--- chaincode/fabcar/java/src/main/java/kr/ac/postech/sslab/nft/BaseNFT.java
+++ chaincode/fabcar/java/src/main/java/kr/ac/postech/sslab/nft/BaseNFT.java
@@ -1,83 +1,133 @@
 package kr.ac.postech.sslab.nft;
 
-import kr.ac.postech.sslab.adapter.ITypeNFT;
+import kr.ac.postech.sslab.adapter.XAtt;
+import kr.ac.postech.sslab.type.Operator;
+import kr.ac.postech.sslab.type.URI;
 import org.hyperledger.fabric.shim.ChaincodeStub;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-public class BaseNFT implements ITypeNFT {
-    private String id;
-    private String owner;
-    private String operator;
-    private String uri;
+import java.util.*;
 
-    @Override
-    public void mint(ChaincodeStub stub, String id, String owner, String uri) {
+public class BaseNFT {
+    private String id;
+    private String type;
+    private String owner;
+    private Operator operator;
+    private String approved;
+    private XAtt xatt;
+    private URI uri;
+
+    public BaseNFT() {}
+
+    private BaseNFT(String id, String type, String owner, Operator operator, String approved, XAtt xatt, URI uri) {
         this.id = id;
+        this.type = type;
         this.owner = owner;
-        this.operator = "";
-        this.uri = uri == null ? "" : uri;
+        this.operator = operator;
+        this.approved = approved;
+        this.xatt = xatt;
+        this.uri = uri;
+    }
+
+    public void mint(ChaincodeStub stub, String id, String type, String owner, String xatt, String uri) throws ParseException {
+        this.id = id;
+        this.type = type;
+        this.owner = owner;
+        this.operator = new Operator();
+        this.approved = "";
+        this.xatt = new XAtt(xatt, this.type);
+        this.uri = new URI(uri);
 
         stub.putStringState(this.id, this.toJSONString());
     }
 
-    @Override
-    public void burn(ChaincodeStub stub, String id) {
-        stub.delState(id);
+    public void burn(ChaincodeStub stub, String tokenId) {
+        stub.delState(tokenId);
     }
 
-    @Override
-    public String read(ChaincodeStub stub, String id) throws ParseException {
-        JSONParser parser = new JSONParser();
-        JSONObject object = (JSONObject) parser.parse(stub.getStringState(id));
+    public static BaseNFT read(ChaincodeStub stub, String id) throws ParseException {
+        JSONObject object = (JSONObject) new JSONParser().parse(stub.getStringState(id));
 
-        this.id = object.get("id").toString();
-        this.owner = object.get("owner").toString();
-        this.operator = object.get("operator").toString();
-        this.uri = object.get("uri").toString();
+        String _id = object.get("id").toString();
+        String _type = object.get("type").toString();
+        String _owner = object.get("owner").toString();
+        Operator _operator = Operator.toList(object.get("operators").toString());
+        String _approved = object.get("approved").toString();
+        XAtt _xatt = new XAtt(object.get("xatt").toString(), _type);
+        URI _uri = new URI(object.get("uri").toString());
 
-        return this.toJSONString();
+        return new BaseNFT(_id, _type, _owner, _operator, _approved, _xatt, _uri);
     }
 
-    @Override
+    public String getId() {
+        return this.id;
+    }
+
+    public String getType() {
+        return this.type;
+    }
+
     public void setOwner(ChaincodeStub stub, String owner) {
         this.owner = owner;
         stub.putStringState(this.id, this.toJSONString());
     }
 
-    @Override
     public String getOwner() {
         return this.owner;
     }
 
-    @Override
-    public void setOperator(ChaincodeStub stub, String operator) {
+    public void setOperator(ChaincodeStub stub, Operator operator) {
         this.operator = operator;
         stub.putStringState(this.id, this.toJSONString());
     }
 
-    @Override
-    public String getOperator() {
+    public Operator getOperator() {
         return this.operator;
     }
 
+    public void setApproved(ChaincodeStub stub, String approved) {
+        this.approved = approved;
+        stub.putStringState(this.id, this.toJSONString());
+    }
 
-    @Override
-    public void setUri(ChaincodeStub stub, String uri) {
+    public String getApproved() {
+        return this.approved;
+    }
+
+    public void setXAtt(ChaincodeStub stub, XAtt xatt) {
+        this.xatt = xatt;
+        stub.putStringState(this.id, this.toJSONString());
+    }
+
+    public XAtt getXAtt() {
+        return this.xatt;
+    }
+
+    public void setUri(ChaincodeStub stub, URI uri) {
         this.uri = uri;
         stub.putStringState(this.id, this.toJSONString());
     }
 
-    @Override
-    public String getUri() {
+    public URI getUri() {
         return this.uri;
     }
 
+    private Map<String, String> toMap() {
+        Map<String, String> map = new HashMap<>();
+        map.put("id", this.id);
+        map.put("type", this.type);
+        map.put("owner", this.owner);
+        map.put("operator", this.operator.toString());
+        map.put("approved", this.approved);
+        map.put("xatt", this.xatt.toJSONString());
+        map.put("uri", this.uri.toJSONString());
+
+        return map;
+    }
+
     private String toJSONString() {
-        return "{\"id\":\"" + this.id + "\","
-                + "\"owner\":\"" + this.owner + "\","
-                + "\"operator\":\"" + this.operator + "\","
-                + "\"uri\":\"" + this.uri + "\"}";
+        return new JSONObject(this.toMap()).toJSONString();
     }
 }
