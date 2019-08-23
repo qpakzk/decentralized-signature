79c3662cef7fa745b9bfe80864791aa1b2dac37b
Sangwon Hong <qpakzk@gmail.com>
2019-08-23 21:58:50 +0900

    define type class Operator

---
+++ chaincode/fabcar/java/src/main/java/kr/ac/postech/sslab/type/Operator.java
@@ -0,0 +1,47 @@
+package kr.ac.postech.sslab.type;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Operator {
+    List<String> operators;
+
+    public Operator() {
+        this.operators = new ArrayList<>();
+    }
+
+    public Operator(List<String> operators) {
+        this.operators = operators;
+    }
+
+    @Override
+    public String toString() {
+        String result = "";
+
+        if(this.operators.size() > 0) {
+            for (String operator : this.operators) {
+                result += operator;
+                result += ",";
+            }
+
+            result = result.substring(0, result.length() - 1);
+        }
+
+        return result;
+    }
+
+    public static Operator toList(String string) {
+        return new Operator(new ArrayList<>(Arrays.asList(string.split(","))));
+    }
+
+    public Operator add(String operator) {
+        this.operators.add(operator);
+        return new Operator(this.operators);
+    }
+
+    public Operator remove(String operator) {
+        this.operators.remove(operator);
+        return new Operator(this.operators);
+    }
+}
