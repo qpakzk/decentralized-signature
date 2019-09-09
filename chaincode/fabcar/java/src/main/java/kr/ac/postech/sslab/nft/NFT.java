c4f360f4fcb9b3756027528d941a860845abd58f
Sangwon Hong <qpakzk@gmail.com>
2019-09-10 00:34:37 +0900

    update NFT structure and rename some attributes

--- chaincode/fabcar/java/src/main/java/kr/ac/postech/sslab/nft/NFT.java
+++ chaincode/fabcar/java/src/main/java/kr/ac/postech/sslab/nft/NFT.java
@@ -1,6 +1,6 @@
 package kr.ac.postech.sslab.nft;
 
-import kr.ac.postech.sslab.adapter.XAtt;
+import kr.ac.postech.sslab.adapter.XAttr;
 import kr.ac.postech.sslab.type.Operator;
 import kr.ac.postech.sslab.type.URI;
 import org.hyperledger.fabric.shim.ChaincodeStub;
@@ -15,50 +15,49 @@ public class NFT {
     private String type;
     private String owner;
     private Operator operator;
-    private String approved;
-    private XAtt xatt;
+    private String approvee;
+    private XAttr xattr;
     private URI uri;
 
     public NFT() {}
 
-    private NFT(String id, String type, String owner, Operator operator, String approved, XAtt xatt, URI uri) {
+    private NFT(String id, String type, String owner, Operator operator, String approvee, XAttr xattr, URI uri) {
         this.id = id;
         this.type = type;
         this.owner = owner;
         this.operator = operator;
-        this.approved = approved;
-        this.xatt = xatt;
+        this.approvee = approvee;
+        this.xattr = xattr;
         this.uri = uri;
     }
 
-    public void mint(ChaincodeStub stub, String id, String type, String owner, String xatt, String uri) throws ParseException {
+    public void mint(ChaincodeStub stub, String id, String type, String owner, XAttr xattr, URI uri) {
         this.id = id;
         this.type = type;
         this.owner = owner;
         this.operator = new Operator();
-        this.approved = "";
-        this.xatt = new XAtt(xatt, this.type);
-        this.uri = new URI(uri);
+        this.approvee = "";
+        this.xattr = xattr;
+        this.uri = uri;
 
         stub.putStringState(this.id, this.toJSONString());
     }
 
-    public void burn(ChaincodeStub stub, String tokenId) {
-        stub.delState(tokenId);
+    public void burn(ChaincodeStub stub, String id) {
+        stub.delState(id);
     }
 
     public static NFT read(ChaincodeStub stub, String id) throws ParseException {
         JSONObject object = (JSONObject) new JSONParser().parse(stub.getStringState(id));
 
-        String _id = object.get("id").toString();
-        String _type = object.get("type").toString();
-        String _owner = object.get("owner").toString();
-        Operator _operator = Operator.toList(object.get("operator").toString());
-        String _approved = object.get("approved").toString();
-        XAtt _xatt = new XAtt(object.get("xatt").toString(), _type);
-        URI _uri = new URI(object.get("uri").toString());
+        String type = object.get("type").toString();
+        String owner = object.get("owner").toString();
+        Operator operator = Operator.toList(object.get("operator").toString());
+        String approvee = object.get("approvee").toString();
+        XAttr xattr = object.get("xattr").toString().equals("") ? new XAttr(type) :  new XAttr(object.get("xattr").toString(), type);
+        URI uri = new URI(object.get("uri").toString());
 
-        return new NFT(_id, _type, _owner, _operator, _approved, _xatt, _uri);
+        return new NFT(id, type, owner, operator, approvee, xattr, uri);
     }
 
     public String getId() {
@@ -87,35 +86,56 @@ public class NFT {
         return this.operator;
     }
 
-    public void setApproved(ChaincodeStub stub, String approved) {
-        this.approved = approved;
+    public void setApprovee(ChaincodeStub stub, String approvee) {
+        this.approvee = approvee;
         stub.putStringState(this.id, this.toJSONString());
     }
 
-    public String getApproved() {
-        return this.approved;
+    public String getApprovee() {
+        return this.approvee;
     }
 
-    public void setXAtt(ChaincodeStub stub, XAtt xatt) {
-        this.xatt = xatt;
+    public void setXAttr(ChaincodeStub stub, XAttr xattr) {
+        this.xattr = xattr;
         stub.putStringState(this.id, this.toJSONString());
     }
 
-    public XAtt getXAtt() {
-        return this.xatt;
+    public XAttr getXAttr() {
+        return this.xattr;
     }
 
-    public void setUri(ChaincodeStub stub, URI uri) {
-        this.uri = uri;
+    public void setURI(ChaincodeStub stub, int index, String attribute) throws Throwable {
+        switch (index) {
+            case 0:
+                this.uri.setPath(attribute);
+                break;
+
+            case 1:
+                this.uri.setHash(attribute);
+                break;
+
+            default:
+                throw new Throwable("Incorrect index. Expecting 0 or 1");
+        }
+
         stub.putStringState(this.id, this.toJSONString());
     }
 
-    public URI getUri() {
+    public URI getURI() {
         return this.uri;
     }
 
-    public boolean checker() {
-        return this.xatt.checker();
+    public String getURI(int index) throws Throwable {
+        switch (index) {
+            case 0:
+                return uri.getPath();
+
+            case 1:
+                return uri.getHash();
+
+            default:
+                throw new Throwable("Incorrect index. Expecting 0 or 1");
+        }
     }
 
     private String toJSONString() {
@@ -124,8 +144,8 @@ public class NFT {
         map.put("type", this.type);
         map.put("owner", this.owner);
         map.put("operator", this.operator.toString());
-        map.put("approved", this.approved);
-        map.put("xatt", this.xatt.toJSONString());
+        map.put("approvee", this.approvee);
+        map.put("xattr", this.xattr.toJSONString());
         map.put("uri", this.uri.toJSONString());
 
         return new JSONObject(map).toJSONString();
