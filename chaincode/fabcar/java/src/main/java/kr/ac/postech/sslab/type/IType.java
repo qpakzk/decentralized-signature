b438c3643bf12362cf6bec72779c07ced43e1d03
Sangwon Hong <qpakzk@gmail.com>
2019-09-17 03:38:55 +0900

    unify interface functions

--- chaincode/fabcar/java/src/main/java/kr/ac/postech/sslab/type/IType.java
+++ chaincode/fabcar/java/src/main/java/kr/ac/postech/sslab/type/IType.java
@@ -1,6 +1,10 @@
 package kr.ac.postech.sslab.type;
 
+import java.util.List;
+
 public interface IType {
+    void assign(List<String> args);
+    void setXAttr(int index, String attr);
+    String getXAttr(int index);
     String toJSONString();
-    String getType();
 }
