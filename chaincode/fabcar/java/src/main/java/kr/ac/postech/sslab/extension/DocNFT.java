e260f6dabb979bd7d511c1cef819b66a18aedf58
Sangwon Hong <qpakzk@gmail.com>
2019-09-17 04:05:39 +0900

    implement DocNFT

--- chaincode/fabcar/java/src/main/java/kr/ac/postech/sslab/extension/DocNFT.java
+++ chaincode/fabcar/java/src/main/java/kr/ac/postech/sslab/extension/DocNFT.java
@@ -1,4 +1,90 @@
 package kr.ac.postech.sslab.extension;
 
-public class DocNFT {
+import kr.ac.postech.sslab.adapter.XAttr;
+import kr.ac.postech.sslab.nft.NFT;
+import kr.ac.postech.sslab.standard.BaseNFT;
+import kr.ac.postech.sslab.type.URI;
+import org.hyperledger.fabric.shim.Chaincode.Response;
+import org.hyperledger.fabric.shim.ChaincodeStub;
+import org.hyperledger.fabric.shim.ResponseUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class DocNFT extends BaseNFT implements IXNFT {
+    //write
+    @Override
+    public Response mint(ChaincodeStub stub, List<String> args) {
+        try {
+            if (args.size() != 7) {
+                throw new Throwable("Incorrect number of arguments. Expecting 7");
+            }
+
+            String id = args.get(0).toLowerCase();
+            String type = args.get(1).toLowerCase();
+            String owner = args.get(2).toLowerCase();
+            String hash = args.get(3).toLowerCase();
+            String signers = args.get(4).toLowerCase();
+
+            XAttr xattr = new XAttr();
+            List<String> list = new ArrayList<>();
+            list.add(type);
+
+
+            list.add(Long.toString(new Random().nextLong()));
+            list.add(id);
+
+            list.add(hash);
+            list.add(signers);
+
+            xattr.assign(list);
+
+            URI uri = new URI(args.get(5).toLowerCase(), args.get(6).toLowerCase());
+
+            NFT nft = new NFT();
+            nft.mint(stub, id, type, owner, xattr, uri);
+
+            return ResponseUtils.newSuccessResponse("Mint a token " + nft.getId());
+        } catch (Throwable throwable) {
+            return ResponseUtils.newErrorResponse(throwable.getMessage());
+        }
+    }
+
+
+    public Response setXAttr(ChaincodeStub stub, List<String> args) {
+        try {
+            if (args.size() != 3) {
+                throw new Throwable("Incorrect number of arguments. Expecting 3");
+            }
+
+            String id = args.get(0).toLowerCase();
+            int index = Integer.parseInt(args.get(1));
+            String attr  = args.get(2).toLowerCase();
+
+            NFT nft = NFT.read(stub, id);
+            nft.setXAttr(stub, index, attr);
+
+            return ResponseUtils.newSuccessResponse("Set a token attribute");
+        } catch (Throwable throwable) {
+            return ResponseUtils.newErrorResponse(throwable.getMessage());
+        }
+    }
+
+    public Response getXAttr(ChaincodeStub stub, List<String> args) {
+        try {
+            if (args.size() != 2) {
+                throw new Throwable("Incorrect number of arguments. Expecting 2");
+            }
+
+            String id = args.get(0).toLowerCase();
+            int index = Integer.parseInt(args.get(1));
+
+            NFT nft = NFT.read(stub, id);
+
+            return ResponseUtils.newSuccessResponse(nft.getXAttr(index));
+        } catch (Throwable throwable) {
+            return ResponseUtils.newErrorResponse(throwable.getMessage());
+        }
+    }
 }
