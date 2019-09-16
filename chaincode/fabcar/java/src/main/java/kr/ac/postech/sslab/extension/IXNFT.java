d506fa487f6e255c7b6afeefea16808ef3cee6fc
Sangwon Hong <qpakzk@gmail.com>
2019-09-17 04:04:34 +0900

    define interface for extended nft

--- chaincode/fabcar/java/src/main/java/kr/ac/postech/sslab/extension/IXNFT.java
+++ chaincode/fabcar/java/src/main/java/kr/ac/postech/sslab/extension/IXNFT.java
@@ -1,4 +1,11 @@
 package kr.ac.postech.sslab.extension;
 
+import org.hyperledger.fabric.shim.Chaincode.Response;
+import org.hyperledger.fabric.shim.ChaincodeStub;
+
+import java.util.List;
+
 public interface IXNFT {
+    Response setXAttr(ChaincodeStub stub, List<String> args);
+    Response getXAttr(ChaincodeStub stub, List<String> args);
 }
